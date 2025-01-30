3ab71c8f75aa5d0f5705f12d5dab5d35db033fd7
Hamid <hamid.elgendy@gmail.com>
2025-01-31 00:24:59 +0200

    feat(react): add path resolution for `arto` in `vite.config.ts`

--- examples/react/vite.config.ts
+++ examples/react/vite.config.ts
@@ -1,6 +1,16 @@
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      // Uncomment the following line to use the built version instead of the source
+      arto: resolve(__dirname, '../../packages/arto/src/index.ts'),
+    },
+  },
 })
